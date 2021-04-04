8cda47c86811b2d6d9c310006314a8c16ff767a1
khoaphamce <phamkhoaa18@gmail.com>
2021-04-04 12:26:59 +0700

    Make description update everytime open

--- config.py
+++ config.py
@@ -1,10 +1,25 @@
 import pandas as pd
 import Backend
 import cv2
+import csv
+
+DT = Backend.Data('NameAndNodes.csv', 'NodesAndCoord.csv', 'NodesAndDistance.csv')
+NameAndNodes = DT.NameNodes()
+descript_list = []
+des_len = len(NameAndNodes["Name"])
+descript_list = [[str(i+1), NameAndNodes["Name"][i]] for i in range(0, des_len)]
+descript_list.insert(0, ["no", "place"])
+
+with open("description.csv", mode='w') as csv_file:
+    writer = csv.writer(csv_file)
+    # writer.writeheader()
+    
+    writer.writerows(descript_list)
+    
+    csv_file.close()
 
 data = pd.read_csv('description.csv')
 BUILDING_LIST = tuple(list(data['place'])) 
-DT = Backend.Data('NameAndNodes.csv', 'NodesAndCoord.csv', 'NodesAndDistance.csv')
 NodesAndDistance = DT.NodesDistance()
 NodesAndCoord = DT.NodesCoord()
 Al = Backend.Algorithm(NodesAndDistance, NodesAndCoord)
